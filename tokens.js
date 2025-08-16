7cf05ad9fbdf6e612ea7bb046dd11ca607119ff5
snooker025 <445114365@tvtc.edu.sa>
2025-08-16 18:34:56 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -4,7 +4,7 @@ export default [
         channelId: "1277722787196174479",
         serverId: "690961823121408022",
         token: process.env.token1,
-        selfDeaf: false,
+        selfDeaf: true,
         autoReconnect: {
             enabled: true,
             delay: 5, // ثواني
