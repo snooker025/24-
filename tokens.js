81924fff3a1c51ce975c57263b2794fb64b97c84
snooker025 <445114365@tvtc.edu.sa>
2025-08-18 12:50:43 +0300

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,7 +1,7 @@
 // status can be "online", "idle", "dnd", or "invisible" or "offline"
 export default [
     {
-        channelId: "1406340284765437982",
+        channelId: "1282264676725424180",
         serverId: "690961823121408022",
         token: process.env.token1,
         selfDeaf: true,
